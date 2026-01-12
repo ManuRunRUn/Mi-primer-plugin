d9ee2602820871aff4fbd100be049128ece3ca32
ManuRunRUn <manuelmoya731@gmail.com>
2026-01-12 15:48:04 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,24 +3,24 @@
 #include <nsysnet/socket.h>
 #include <string.h>
 
-// Puerto definido en tu receptor_pro.py
-#define PORT 5001 
+#define PORT_PYTHON 5001
 
 WUPS_PLUGIN_NAME("XtremeStreamer");
 WUPS_PLUGIN_AUTHOR("User");
 WUPS_PLUGIN_VERSION("1.0");
 
-static int video_sock = -1;
-static struct sockaddr_in addr;
+static int sock = -1;
+static struct sockaddr_in app_addr;
 
 WUPS_INITIALIZE() {
     socket_lib_init();
-    video_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = 0xFFFFFFFF; // Broadcast para localizacion
+    // Configurar para que responda a cualquier PC que lo busque
+    memset(&app_addr, 0, sizeof(app_addr));
+    app_addr.sin_family = AF_INET;
+    app_addr.sin_port = htons(PORT_PYTHON);
+    app_addr.sin_addr.s_addr = 0xFFFFFFFF; // Broadcast
 
     return WUPS_INIT_OK;
 }
@@ -28,9 +28,9 @@ WUPS_INITIALIZE() {
 DECL_FUNCTION(void, GX2CopyScanBuffer, void* buffer, uint32_t target) {
     real_GX2CopyScanBuffer(buffer, target);
 
-    if (video_sock >= 0 && buffer != NULL) {
-        // Envia el ID que espera tu receptor_pro.py
-        sendto(video_sock, "WIIU_ID", 7, 0, (struct sockaddr*)&addr, sizeof(addr));
+    if (sock >= 0 && buffer != NULL) {
+        // espera recibir "WIIU_ID"
+        sendto(sock, "WIIU_ID", 7, 0, (struct sockaddr*)&app_addr, sizeof(app_addr));
     }
 }
 
