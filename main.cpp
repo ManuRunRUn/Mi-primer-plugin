0ca7874afa3091e68fc088d74fd001558b7a992b
ManuRunRUn <manuelmoya731@gmail.com>
2026-01-12 15:38:18 -0400

    Refactor socket initialization and buffer handling

--- main.cpp
+++ main.cpp
@@ -1,37 +1,36 @@
 #include <wups.h>
+#include <gx2/common.h>
 #include <nsysnet/socket.h>
 #include <string.h>
 
-// El puerto que configuraste en tu código Python
-#define PORT 5001 
-
-static int s = -1;
-static struct sockaddr_in addr;
+#define PORT_APP 5001 
 
 WUPS_PLUGIN_NAME("XtremeStreamer");
 WUPS_PLUGIN_AUTHOR("User");
 WUPS_PLUGIN_VERSION("1.0");
 
+static int video_sock = -1;
+static struct sockaddr_in app_addr;
+
 WUPS_INITIALIZE() {
     socket_lib_init();
-    s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    video_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
-    // Configurar dirección para el broadcast
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = 0xFFFFFFFF; // Manda a todos para que tu app lo vea
+    // Configurar dirección de envío
+    memset(&app_addr, 0, sizeof(app_addr));
+    app_addr.sin_family = AF_INET;
+    app_addr.sin_port = htons(PORT_APP);
+    app_addr.sin_addr.s_addr = 0xFFFFFFFF; // Broadcast para que tu receptor_pro.py lo vea
 
     return WUPS_INIT_OK;
 }
 
-// Esta función es la que captura la pantalla
 DECL_FUNCTION(void, GX2CopyScanBuffer, void* buffer, uint32_t target) {
     real_GX2CopyScanBuffer(buffer, target);
 
-    if (s >= 0 && buffer != NULL) {
-        // Enviamos el ID que tu aplicación receptor_pro.py está esperando
-        sendto(s, "WIIU_ID", 7, 0, (struct sockaddr*)&addr, sizeof(addr));
+    if (video_sock >= 0 && buffer != NULL) {
+        // espera "WIIU_ID"
+        sendto(video_sock, "WIIU_ID", 7, 0, (struct sockaddr*)&app_addr, sizeof(app_addr));
     }
 }
 
