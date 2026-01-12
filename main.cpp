64f2b5b702d2dadf621d7906f12a2488852a4fdc
ManuRunRUn <manuelmoya731@gmail.com>
2026-01-12 02:56:14 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,26 @@
 #include <wups.h>
 #include <gx2/common.h>
-#include <nsysnet/socket.h> // Red oficial de Wii U (Nativo)
+#include <nsysnet/socket.h>
 #include <string.h>
-#include <stdint.h>
 #include <coreinit/thread.h>
 
-#define PORT_VIDEO 5000
+#define PORT_APP 5001
 
 static int video_sock = -1;
-static struct sockaddr_in laptop_addr;
+static struct sockaddr_in app_addr;
+static bool app_connected = false;
 
 WUPS_PLUGIN_NAME("XtremeStreamer");
 WUPS_PLUGIN_AUTHOR("User");
-WUPS_PLUGIN_VERSION("1.0");
-WUPS_PLUGIN_LICENSE("GPL");
+WUPS_PLUGIN_VERSION("2.1");
 
 WUPS_INITIALIZE() {
-    // Inicializar la red de la consola
     socket_lib_init();
-
     video_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    if (video_sock < 0) return WUPS_INIT_FAILED;
-
-    memset(&laptop_addr, 0, sizeof(laptop_addr));
-    laptop_addr.sin_family = AF_INET;
-    laptop_addr.sin_port = htons(PORT_VIDEO);
     
-    // Broadcast para que tu programa de Windows encuentre la consola
-    laptop_addr.sin_addr.s_addr = 0xFFFFFFFF; 
-
-    int broadcastEnable = 1;
-    setsockopt(video_sock, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable));
+    // Modo no bloqueante para no congelar el juego
+    int enable = 1;
+    setsockopt(video_sock, SOL_SOCKET, SO_NONBLOCK, &enable, sizeof(enable));
 
     return WUPS_INIT_OK;
 }
@@ -38,10 +28,27 @@ WUPS_INITIALIZE() {
 DECL_FUNCTION(void, GX2CopyScanBuffer, void* buffer, uint32_t scanTarget) {
     real_GX2CopyScanBuffer(buffer, scanTarget);
 
-    if (buffer != NULL && video_sock >= 0) {
-        // Enviamos paquetes de 1400 bytes (MTU ideal para Windows)
-        sendto(video_sock, buffer, 1400, 0, (struct sockaddr*)&laptop_addr, sizeof(laptop_addr));
+    if (buffer == NULL || video_sock < 0) return;
+
+    // Escuchar el "Ping" de tu app de Python
+    char recv_buf[16];
+    struct sockaddr_in sender_addr;
+    int addr_len = sizeof(sender_addr);
+    
+    // Si tu app manda algo, respondemos para que nos detecte
+    int bytes = recvfrom(video_sock, recv_buf, sizeof(recv_buf), 0, (struct sockaddr*)&sender_addr, &addr_len);
+    if (bytes > 0) {
+        // Tu app espera "WIIU_ID", así que se lo mandamos de vuelta
+        sendto(video_sock, "WIIU_ID", 7, 0, (struct sockaddr*)&sender_addr, sizeof(sender_addr));
+        app_addr = sender_addr;
+        app_addr.sin_port = htons(PORT_APP);
+        app_connected = true;
     }
-} 
+
+    // Si ya nos detectó, empezamos a mandar la imagen
+    if (app_connected) {
+        sendto(video_sock, buffer, 1400, 0, (struct sockaddr*)&app_addr, sizeof(app_addr));
+    }
+}
 
 WUPS_MUST_REPLACE(GX2CopyScanBuffer, WUPS_LOADER_LIBRARY_GX2, GX2CopyScanBuffer);
